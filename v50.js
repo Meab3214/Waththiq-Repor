b4a55dad65c434e42762ab2e53c01ef7db9b218c
Vv <lool3214@outlook.com>
2026-08-28 05:43:51 +0300

    V16: bust cache for logo upload support

--- v50.js
+++ v50.js
@@ -2,7 +2,7 @@
 (function(){
  [...document.querySelectorAll('link[rel="stylesheet"]')].filter(x=>/v60|v70|v80|v90|v100|v110|v120|v121|v130|v140|v150/.test(x.href)).forEach(x=>x.remove());
  [...document.querySelectorAll('script[src]')].filter(x=>/v60|v70|v80|v90|v100|v110|v120|v130|v140|v150/.test(x.src)).forEach(x=>x.remove());
- const base=document.createElement('link');base.rel='stylesheet';base.href='./v140.css?v=150';document.head.appendChild(base);
- const visual=document.createElement('link');visual.rel='stylesheet';visual.href='./v150.css?v=150';document.head.appendChild(visual);
- const engine=document.createElement('script');engine.src='./v140.js?v=150';engine.async=false;document.body.appendChild(engine);
+ const base=document.createElement('link');base.rel='stylesheet';base.href='./v140.css?v=160';document.head.appendChild(base);
+ const visual=document.createElement('link');visual.rel='stylesheet';visual.href='./v150.css?v=160';document.head.appendChild(visual);
+ const engine=document.createElement('script');engine.src='./v140.js?v=160';engine.async=false;document.body.appendChild(engine);
 })();
